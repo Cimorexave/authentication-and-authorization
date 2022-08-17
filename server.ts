7140fd0194b4fe8c5a3ca971ed8888cbd417d2f6
Cimorexave <sadeq.s502@gmail.com>
2022-08-17 16:51:50 +0430

    server setup

--- server.ts
+++ server.ts
@@ -1,5 +1,11 @@
 //import express
 import express from "express";
 import {Request, Response} from "express";
+const app = express()
  //config env 
  import "dotenv/config"
+ //Connecting to the Database
+
+//Middleware
+//Parsing json reqs
+app.use(express.json())
