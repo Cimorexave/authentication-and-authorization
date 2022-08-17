d36b9e13951c2737bd88e005ab53d1ffc7dd2e61
Cimorexave <sadeq.s502@gmail.com>
2022-08-17 16:57:35 +0430

    register and login routes setup

---
+++ routes/authentication.ts
@@ -0,0 +1,8 @@
+//import express
+import * as express from "express";
+import {Request, Response} from "express";
+const router = express().Router()
+
+router.post("/register", (req, res) => {
+    
+})
