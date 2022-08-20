51a78b15dcc28f62c498225bd5cef0478fb65053
Cimorexave <sadeq.s502@gmail.com>
2022-08-20 12:41:37 +0430

    fixed express.router bug

--- routes/posts.ts
+++ routes/posts.ts
@@ -1,7 +1,7 @@
 //import express
 import * as express from "express";
 import {Request, Response} from "express";
-const router = express().Router()
+const router = express.Router()
 
 router.post("/posts/", (req, res) => {
     
