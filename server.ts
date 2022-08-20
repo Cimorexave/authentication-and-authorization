62755f07add9227311a5156223b0ff67fa685758
Cimorexave <sadeq.s502@gmail.com>
2022-08-20 12:41:55 +0430

    setting up db connection and bug fix

--- server.ts
+++ server.ts
@@ -1,10 +1,38 @@
 //import express
-import express from "express";
+import * as express from "express";
 import {Request, Response} from "express";
 const app = express()
+//import db accessories
+import User from "./entities/User";
+import { createConnection, DataSource } from "typeorm";
+// createConnection({
+//     type: "postgres",
+//     database: "postgres",
+//     username: "postgres",
+//     password: "changeme",
+//     logging: true,
+//     synchronize: true,
+//     entities: [User]
+// }).then( () => {
+//     console.log("Connected to Database")
+// })
+ //Connecting to the Database
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "changeme",
+    database: "postgres",
+    synchronize: true,
+    logging: true,
+    entities: [User],
+    subscribers: [],
+    migrations: [],
+})
  //config env 
  import "dotenv/config"
- //Connecting to the Database
 
 //Middleware
 //Parsing json reqs
@@ -12,6 +40,11 @@ app.use(express.json())
 //Routes
 import authentication from "routes/authentication";
 import authorization from "routes/authorization";
-import posts from "routes/posts";
-//Protected Route
+import posts from "./routes/posts";
+// Protected Route
 app.use("/api/", posts)
+
+//Running Server
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Server Running on port: ${process.env.SERVER_PORT}`)
+})
