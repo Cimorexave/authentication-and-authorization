32e3a335a8bf628d01135e0403e08aa09ba8a816
Cimorexave <sadeq.s502@gmail.com>
2022-08-20 15:05:13 +0430

    setting up login

--- routes/authorization.ts
+++ routes/authorization.ts
@@ -3,8 +3,41 @@ import * as express from "express";
 import {Request, Response} from "express";
 const router = express.Router()
 
-router.post("/login", (req, res) => {
-    
+//import Bcrypt
+import bcrypt from "bcryptjs";
+
+//import schema
+import User from "../entities/User";
+
+//import datasource
+import { myAppDataSource } from "../server";
+const userRepository = myAppDataSource.getRepository(User)
+
+//Validation
+import {validation as validate} from "../validation/validation"
+
+//GET REQUEST
+router.get("/login", (req: Request, res: Response) => {
+    res.status(200).send("Login User: Send username and password in the body")
+})
+//POST REQUEST
+router.post("/login", async (req: Request, res: Response) => {
+    //User Request Validation
+    const {error} = validate(req.body)
+    if (error) res.status(400).send(error.details[0].message)
+
+    //Checking if the user has registered before
+    const user = await userRepository.findOneBy({
+        name: req.body.username
+    })
+    if (!user) res.status(400).send(`User doesn't exist.`)
+
+    //Check for password
+    const validPassword = bcrypt.compare(req.body.password, user)
+    if(!validPassword) return res.status(400).send("Password is incorrect")
+
+    res.send("Logged in successfully!")
+
 })
 
 export default router
